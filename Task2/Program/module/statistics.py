4056e6d37a4204aedc718839a9322f9970aacedf
Kamil Kowalewski <szamil24@gmail.com>
2021-04-06 23:14:45 +0200

    Add printing base statistics

--- Task2/Program/module/statistics.py
+++ Task2/Program/module/statistics.py
@@ -6,7 +6,12 @@ def calculate_statistics(df: pd.DataFrame) -> None:
         print(df.isna())
         raise MissingValuesException
 
-    print("Średnia\n", df.mean())
+    print("\nMean\n", df.mean())
+    print("\nStandard Deviation\n", df.std())
+    print("\nMode\n", df.mode())
+    print("\nFirst quantile\n", df.quantile([0.25]))
+    print("\nMedian (Second quantile)\n", df.median())
+    print("\nThird quantile\n", df.quantile([0.75]))
 
 
 class MissingValuesException(Exception):
