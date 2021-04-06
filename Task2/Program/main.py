9a9e66c91af55ffbbe0ef44ea79160f83f9be708
Kamil Kowalewski <szamil24@gmail.com>
2021-04-06 22:58:41 +0200

    Small change in displaying

--- Task2/Program/main.py
+++ Task2/Program/main.py
@@ -11,24 +11,31 @@ from module.statistics import calculate_statistics
 
 def main() -> None:
     for ds, label in zip(read([5, 15, 30, 45]), ['5%', '15%', '30%', '45%']):
-        print("------------------------------------------------------------------------")
         print(label, "missing values\n\n")
 
+        display_separator()
         print("List wise deletion")
         calculate_statistics(ds.dropna())
 
+        display_separator()
         print("Mean imputation")
         calculate_statistics(mean(ds))
 
+        display_separator()
         print("Interpolation")
         calculate_statistics(interpolate(ds))
 
+        display_separator()
         print("Hot deck")
         calculate_statistics(hot_deck(ds))
 
+        display_separator()
         print("Regression")
         # calculate_statistics(regression(ds))
-        print("------------------------------------------------------------------------")
+
+
+def display_separator() -> None:
+    print("------------------------------------------------------------------------")
 
 
 def check_types_check_style() -> None:
