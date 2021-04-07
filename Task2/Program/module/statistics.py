e151b9f82709f32c9a126513afc755045b66fa80
Kamil Kowalewski <szamil24@gmail.com>
2021-04-07 10:04:16 +0200

    Refactor in statistics.py

--- Task2/Program/module/statistics.py
+++ Task2/Program/module/statistics.py
@@ -6,11 +6,35 @@ def calculate_statistics(df: pd.DataFrame, save_tables: bool) -> None:
         print(df.isna())
         raise MissingValuesException
 
+    calculate_mean(df)
+    calculate_std(df)
+    calculate_mode(df)
+    calculate_first_quantile(df)
+    calculate_median(df)
+    calculate_third_quantile(df)
+
+
+def calculate_mean(df):
     print("\nMean\n", df.mean())
+
+
+def calculate_std(df):
     print("\nStandard Deviation\n", df.std())
+
+
+def calculate_mode(df):
     print("\nMode\n", df.mode())
+
+
+def calculate_first_quantile(df):
     print("\nFirst quantile\n", df.quantile([0.25]))
+
+
+def calculate_median(df):
     print("\nMedian (Second quantile)\n", df.median())
+
+
+def calculate_third_quantile(df):
     print("\nThird quantile\n", df.quantile([0.75]))
 
 
