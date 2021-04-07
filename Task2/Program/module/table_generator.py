a78081535b469b20f05614970933d3e10fb4a905
Kamil Kowalewski <szamil24@gmail.com>
2021-04-07 11:36:18 +0200

    Finish table_generator.py

--- Task2/Program/module/table_generator.py
+++ Task2/Program/module/table_generator.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 
 
@@ -5,13 +6,16 @@ def generate_table(header: List[str], content: List[str], filename: str) -> None
     if len(header) != len(content):
         raise Exception("Lists must have equal length and length must be equals 2!!!")
 
-    begin: str = "\\begin{table}[]\n\\begin{tabular}{|c|c|c|c|c|}\n\hline\n"
-    end: str = "\end{tabular}\n\end{table}\n"
+    begin: str = "\\begin{table}[]\n\centering\n\\begin{tabular}{|c|c|c|c|c|}\n\hline\n"
+    table_description = "\n\caption{TODO}\n\label{" + filename + "}"
+    end: str = "\end{tabular}\n" + table_description + "\end{table}\n"
     result = begin
 
     for index in range(len(header)):
         result += header[index] + " & " + str(content[index]) + " \\\ \hline\n"
 
     result += end
-    with open(filename + ".txt", "w") as file:
+
+    current_time = datetime.now().strftime("%H%M%S")
+    with open(filename + current_time + ".txt", "w") as file:
         file.write(result)
