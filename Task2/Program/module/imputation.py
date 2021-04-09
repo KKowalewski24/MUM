da5184fa4a96832c23506834c5a6acf176146631
sgruda <szymon.gruda98@gmail.com>
2021-04-09 20:51:56 +0200

    Add LogisticRegression

--- Task2/Program/module/imputation.py
+++ Task2/Program/module/imputation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.impute import KNNImputer
 # from sklearn.cross_validation import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 
 def mean(df: pd.DataFrame) -> pd.DataFrame:
     return df.fillna(df.mean())
@@ -26,24 +26,26 @@ def regression(df: pd.DataFrame) -> pd.DataFrame:
     not_fuzzy_headers = ['sex','chest-pain-type','fasting-blood-sugar','resting-electrocardiographic-results','exercise-induced-angina','the-slope-of-the-peak-exercise','number-of-major-vessels','thal','target']
     i = 0
     # [ 'age','resting-blood-pressure','serum-cholestoral, 'maximum-heart-rate','oldpeak']
-    for header in fuzzy_headers:
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-            print(df)
+    for header in headers:
         df_to_regression_model = df.dropna(subset = headers)
         df_to_regression_model = df_to_regression_model.loc[:, headers]
         headers.remove(header)
         x = df_to_regression_model[headers]
         y = df_to_regression_model[header]
 
-        lm = LinearRegression().fit(x, y)
+        if header in fuzzy_headers:
+            lm = LinearRegression().fit(x, y)
+        elif header in not_fuzzy_headers:
+            lm = LogisticRegression(max_iter=1000000000000).fit(x, y)
+
 
         temp = interpolate(df)
         temp[header] = df[header]
 
-        # temp[temp.isnull().any(axis=1)] #zostawia tylko wiersze z jakims nullem
-        is_NaN = temp.isnull()
-        row_has_NaN = is_NaN.any(axis=1)
-        temp = temp[row_has_NaN]
+        temp[temp.isnull().any(axis=1)] #zostawia tylko wiersze z jakims nullem
+        # is_NaN = temp.isnull()
+        # row_has_NaN = is_NaN.any(axis=1)
+        # temp = temp[row_has_NaN]
 
 
         del temp[header]
