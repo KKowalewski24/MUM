6d96f81a671688b30d37f612882eca479591cd2e
Kamil Kowalewski <szamil24@gmail.com>
2021-04-09 18:30:29 +0200

    Fix creating tables

--- Task2/Program/module/latex_generator.py
+++ Task2/Program/module/latex_generator.py
@@ -1,28 +1,31 @@
-import pandas as pd
 from datetime import datetime
-from typing import List
+
+import pandas as pd
 
 RESULTS_DIR_NAME = "results"
 
 
 def generate_table(df: pd.DataFrame, filename: str) -> None:
-    # TODO
-    if len(header) != len(content):
-        raise Exception("Lists must have equal length!!!")
+    begin: str = "\\begin{table}[!htbp]\n"
+    centering: str = "\centering\n"
+    begin_tabular: str = "\\begin{tabular}{|c|c|c|c|c|c|c|}\n"
+    back_slashes: str = "\\\\"
+    hline: str = "\hline\n"
+    end_tabular: str = "\end{tabular}\n"
+    end: str = "\end{table}\n"
+    result = begin + centering + begin_tabular + hline
+    # result += " & " + back_slashes + " " + hline
+    for i in range(len(df.values)):
+        result += df.index[i] + " & "
+        for j in range(len(df.values[i])):
+            result += str(round(df.values[i].tolist()[j], 4))
+            if j < len(df.values[i]) - 1:
+                result += " & "
+        result += " " + back_slashes + " " + hline
 
-    begin: str = "\\begin{table}[!htbp]\n\centering\n\\begin{tabular}{|c|c|c|c|c|}\n\hline\n"
-    table_description = "\n\caption{TODO}\n\label{" + filename.replace("%", "") + "}\n"
-    end: str = "\end{tabular}" + table_description + "\end{table}\n"
-    result = begin
+    result += end_tabular + end
 
-    for index in range(len(header)):
-        result += str(header[index]) + " & " + str(content[index]) + " \\\ \hline\n"
-
-    result += end
-
-    current_time = datetime.now().strftime("%H%M%S")
-    with open(RESULTS_DIR_NAME + "/" + filename + current_time + ".txt", "w") as file:
-        file.write(result)
+    print(result)
 
 
 def generate_image_figure(image_filename: str) -> None:
@@ -35,3 +38,8 @@ def generate_image_figure(image_filename: str) -> None:
     path = RESULTS_DIR_NAME + "/figure_" + image_filename + current_time + ".txt"
     with open(path, "w") as file:
         file.write(begin + middle + end)
+
+
+def save_to_file(self, data: str, filename: str) -> None:
+    with open(filename + "-" + datetime.now().strftime("%H%M%S"), "w") as save_file:
+        save_file.write(data)
