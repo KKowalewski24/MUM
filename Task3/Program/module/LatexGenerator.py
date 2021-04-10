8729cddb37b20ef62fdda48fba19322fe645c95b
Kamil Kowalewski <szamil24@gmail.com>
2021-04-10 09:38:16 +0200

    Initial commit of task3

---
+++ Task3/Program/module/LatexGenerator.py
@@ -0,0 +1,205 @@
+import os
+from datetime import datetime
+from typing import List
+
+import pandas as pd
+
+
+class LatexItem:
+    ampersand: str = " & "
+    centering: str = "\centering\n"
+    float_barrier: str = "\FloatBarrier\n"
+
+
+class Table(LatexItem):
+    begin: str = "\\begin{table}[!htbp]\n"
+    back_slashes: str = "\\\\"
+    hline: str = "\hline\n"
+    end_tabular: str = "\end{tabular}\n"
+    end: str = "\end{table}\n"
+
+
+    def get_begin_tabular(self, table_width: int) -> str:
+        columns: str = ""
+        for i in range(table_width):
+            columns += "|c"
+        columns += "|"
+        return "\\begin{tabular}{" + columns + "}\n"
+
+
+class Image(LatexItem):
+    begin: str = "\\begin{figure}[!htbp]\n"
+    include: str = "\includegraphics\n"
+    width: str = "[width=\\textwidth,keepaspectratio]\n"
+    end: str = "\end{figure}"
+
+
+    def __init__(self, directory_name: str) -> None:
+        self.directory_name = directory_name
+
+
+    def get_path(self, filename: str) -> str:
+        return self.directory_name + "/" + filename
+
+
+    def get_latex_path(self, filename: str) -> str:
+        return "{" + self.get_path(filename) + ".png}"
+
+
+    def get_caption(self, text: str) -> str:
+        return "\caption\n[" + text + "]{" + text + "}"
+
+
+    def get_label(self, label: str) -> str:
+        return "\label{" + label + "}"
+
+
+class LatexGenerator:
+
+    def __init__(self, dir_name: str = "") -> None:
+        self.dir_name = dir_name
+        self.table = Table()
+        self.image = Image(dir_name)
+
+
+    def generate_vertical_table_df(self, df: pd.DataFrame, filename: str) -> None:
+        result: str = self.table.begin + self.table.centering \
+                      + self.table.get_begin_tabular(len(df.columns)) + self.table.hline
+
+        header: str = ""
+        for i in range(len(df.columns)):
+            header += str(df.columns[i])
+            if i < len(df.columns[i]) - 1:
+                header += self.table.ampersand
+
+        header += " " + self.table.back_slashes + " " + self.table.hline
+
+        body: str = ""
+        for i in range(len(df.values)):
+            for j in range(len(df.values[i])):
+                body += str(df.values[i][j])
+                if j < len(df.values[j]) - 1:
+                    body += self.table.ampersand
+            body += " " + self.table.back_slashes + " " + self.table.hline
+
+        result += header + body + self.table.end_tabular \
+                  + self.table.end + self.table.float_barrier
+        self._save_to_file(result, filename)
+
+
+    def generate_vertical_table(self, header_names: List[str],
+                                body_values: List[List[float]],
+                                filename: str) -> None:
+        if len(header_names) != len(body_values):
+            raise Exception("Lists must have equal length")
+
+        result: str = self.table.begin + self.table.centering \
+                      + self.table.get_begin_tabular(len(header_names)) + self.table.hline
+        header: str = ""
+        for i in range(len(header_names)):
+            header += header_names[i]
+            if i < len(header_names) - 1:
+                header += self.table.ampersand
+
+        header += " " + self.table.back_slashes + " " + self.table.hline
+
+        body: str = ""
+        for i in range(len(body_values)):
+            for j in range(len(body_values[i])):
+                body += str(body_values[i][j])
+                if j < len(body_values[i]) - 1:
+                    body += self.table.ampersand
+            body += " " + self.table.back_slashes + " " + self.table.hline
+
+        result += header + body + self.table.end_tabular \
+                  + self.table.end + self.table.float_barrier
+        self._save_to_file(result, filename)
+
+
+    def generate_horizontal_table_df(self, df: pd.DataFrame, filename: str) -> None:
+        result: str = self.table.begin + self.table.centering \
+                      + self.table.get_begin_tabular(len(df.columns) + 1) + self.table.hline
+
+        header: str = ""
+        for i in range(len(df.columns)):
+            header += df.columns[i]
+            if i <= len(df.columns[i]) + 1:
+                header += self.table.ampersand
+
+        result += self.table.ampersand + header + " " \
+                  + self.table.back_slashes + " " + self.table.hline
+
+        body: str = ""
+        for i in range(len(df.values)):
+            body += str(df.index[i]) + self.table.ampersand
+            for j in range(len(df.values[i])):
+                body += str(df.values[i][j])
+                if j < len(df.values[i]) - 1:
+                    body += self.table.ampersand
+
+            body += " " + self.table.back_slashes + " " + self.table.hline
+
+        result += body + self.table.end_tabular + self.table.end + self.table.float_barrier
+        self._save_to_file(result, filename)
+
+
+    def generate_horizontal_table(self, header_names: List[str],
+                                  horizontal_column_names: List[str],
+                                  body_values: List[List[float]], filename: str) -> None:
+        for i in range(len(body_values)):
+            if len(header_names) != len(body_values[i]):
+                raise Exception("header_names and body_values must have equal length")
+
+        if len(horizontal_column_names) != len(body_values):
+            raise Exception(
+                "horizontal_column_names and body_values must have equal length"
+            )
+
+        result: str = self.table.begin + self.table.centering \
+                      + self.table.get_begin_tabular(len(header_names) + 1) + self.table.hline
+
+        header: str = ""
+        for i in range(len(header_names)):
+            header += header_names[i]
+            if i < len(header_names[i]) + 1:
+                header += self.table.ampersand
+
+        result += self.table.ampersand + header + " " \
+                  + self.table.back_slashes + " " + self.table.hline
+
+        body: str = ""
+        for i in range(len(body_values)):
+            body += horizontal_column_names[i] + self.table.ampersand
+            for j in range(len(body_values[i])):
+                body += str(body_values[i][j])
+                if j < len(body_values[i]) - 1:
+                    body += self.table.ampersand
+            body += " " + self.table.back_slashes + " " + self.table.hline
+
+        result += body + self.table.end_tabular + self.table.end + self.table.float_barrier
+        self._save_to_file(result, filename)
+
+
+    def generate_chart_image(self, filename: str) -> None:
+        result: str = self.image.begin + self.image.centering \
+                      + self.image.include + self.image.width
+        result += self.image.get_latex_path(filename)
+        result += self.image.get_caption(self._remove_png_extension(filename))
+        result += self.image.get_label(self._remove_png_extension(filename))
+        self._save_to_file(result, filename)
+
+
+    def _save_to_file(self, data: str, filename: str) -> None:
+        path: str = ""
+        if self.dir_name != "":
+            path = self.dir_name + "/"
+            if not os.path.exists(self.dir_name):
+                os.makedirs(self.dir_name)
+
+        path += filename + "-" + datetime.now().strftime("%H%M%S") + ".txt"
+        with open(path, "w") as file:
+            file.write(data)
+
+
+    def _remove_png_extension(self, string: str) -> str:
+        return string.replace(".png", "")
