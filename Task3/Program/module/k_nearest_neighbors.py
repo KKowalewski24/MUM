5dddf7c7a9a2436ca1cf0682e59c63f7ae684591
Kamil Kowalewski <szamil24@gmail.com>
2021-04-19 16:42:53 +0200

    Add working version of knn

--- Task3/Program/module/k_nearest_neighbors.py
+++ Task3/Program/module/k_nearest_neighbors.py
@@ -1,13 +1,20 @@
 from typing import Tuple
 
 import numpy as np
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
 
 from module.LatexGenerator import LatexGenerator
 
 latex_generator: LatexGenerator = LatexGenerator("knn")
 
+K_RANGE = range(1, 3)
+
 
 def knn_classification(data_set: Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray],
                        save_latex: bool) -> None:
-    # TODO
-    pass
+    for k_number in K_RANGE:
+        knn_classifier = KNeighborsClassifier(n_neighbors=k_number)
+        knn_classifier.fit(data_set[0], data_set[2])
+        y_prediction = knn_classifier.predict(data_set[1])
+        print(metrics.accuracy_score(data_set[3], y_prediction))
