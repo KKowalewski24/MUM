8aa97ba67d2417aa265948ede3e745dbca94799a
Kamil Kowalewski <szamil24@gmail.com>
2021-04-19 16:49:23 +0200

    Add displaying result

--- Task3/Program/module/k_nearest_neighbors.py
+++ Task3/Program/module/k_nearest_neighbors.py
@@ -13,8 +13,9 @@ K_RANGE = range(1, 3)
 
 def knn_classification(data_set: Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray],
                        save_latex: bool) -> None:
-    for k_number in K_RANGE:
-        knn_classifier = KNeighborsClassifier(n_neighbors=k_number)
+    for k_value in K_RANGE:
+        knn_classifier = KNeighborsClassifier(n_neighbors=k_value)
         knn_classifier.fit(data_set[0], data_set[2])
         y_prediction = knn_classifier.predict(data_set[1])
-        print(metrics.accuracy_score(data_set[3], y_prediction))
+        accuracy = metrics.accuracy_score(data_set[3], y_prediction)
+        print("K parameter value: " + str(k_value) + ",\t" + "accuracy: " + str(accuracy))
