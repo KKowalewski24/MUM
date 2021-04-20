ecc571c04710cc45821ec31521dcf05eaa8e1165
Kamil Kowalewski <szamil24@gmail.com>
2021-04-20 17:19:47 +0200

    Add manhattan metric

--- Task3/Program/module/k_nearest_neighbors.py
+++ Task3/Program/module/k_nearest_neighbors.py
@@ -14,11 +14,14 @@ latex_generator: LatexGenerator = LatexGenerator(LATEX_RESULTS_DIR)
 
 
 def knn_classification(data_set: Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray],
-                       save_latex: bool) -> None:
+                       is_euclidean_metric: bool, data_set_name: str,
+                       save_latex: bool = False) -> None:
     accuracy_list: List[List[float]] = []
 
     for k_value in K_RANGE:
-        knn_classifier = KNeighborsClassifier(n_neighbors=k_value)
+        knn_classifier = KNeighborsClassifier(
+            n_neighbors=k_value, p=2 if is_euclidean_metric else 1
+        )
         knn_classifier.fit(data_set[0], data_set[2])
         y_prediction = knn_classifier.predict(data_set[1])
         accuracy = round(metrics.accuracy_score(data_set[3], y_prediction), 4)
@@ -30,11 +33,14 @@ def knn_classification(data_set: Tuple[np.ndarray, np.ndarray, np.ndarray, np.nd
     plt.xlabel("K parameter value")
 
     if save_latex:
+        filename_description = "_" + data_set_name + ("_eucl" if is_euclidean_metric else "_manh")
         latex_generator.generate_horizontal_table(
-            ["Accuracy"], list(K_RANGE), accuracy_list, "knn_table"
+            ["Accuracy"], list(K_RANGE), accuracy_list,
+            "knn_table" + filename_description
         )
-        latex_generator.generate_chart_image("knn_chart")
-        plt.savefig(LATEX_RESULTS_DIR + "/knn_chart" + "-" + datetime.now().strftime("%H%M%S"))
+        image_filename = filename_description + "-" + datetime.now().strftime("%H%M%S")
+        latex_generator.generate_chart_image("knn_chart" + image_filename)
+        plt.savefig(LATEX_RESULTS_DIR + "/knn_chart" + image_filename)
         plt.close()
 
     plt.show()
