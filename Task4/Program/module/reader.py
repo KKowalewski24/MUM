e7fa452553958a9d97f5267badef8894c6118f33
Kamil Kowalewski <szamil24@gmail.com>
2021-04-23 17:34:48 +0200

    Add data sets and reader from them

---
+++ Task4/Program/module/reader.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+TEST_SET_SIZE = 0.3
+RANDOM_STATE_VALUE = 21
+
+
+def read_heart_ds() -> pd.DataFrame:
+    return pd.read_csv('data/heart.csv')
+
+
+def read_gestures_ds() -> pd.DataFrame:
+    return pd.read_csv('data/gestures.csv')
+
+
+def read_weather_AUS_ds() -> pd.DataFrame:
+    df = pd.read_csv('data/weatherAUS.csv').dropna()
+
+    # encode date as day-of-year
+    df['DayOfYear'] = pd.to_datetime(
+        df['Date']).map(lambda date: date.day_of_year)
+    df.drop('Date', axis=1, inplace=True)
+
+    # encode categorical columns
+    categorical_columns = [
+        'Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm', 'RainToday', 'RainTomorrow'
+    ]
+    encoder = LabelEncoder()
+    for column in categorical_columns:
+        df[column] = encoder.fit_transform(df[column])
+
+    return df
