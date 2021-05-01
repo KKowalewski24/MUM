b25affdcef52d272f6c386a7e28860a1c137acb7
Kamil Kowalewski <szamil24@gmail.com>
2021-05-01 13:41:11 +0200

    Rename

--- Task4/Program/module/db_scan.py
+++ Task4/Program/module/db_scan.py
@@ -2,16 +2,16 @@ import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_score
 
-EPSILON_RANGE = np.arange(0.1, 10, 0.1)
 MIN_SAMPLES_RANGE = np.arange(2, 10, 1)
+EPSILON_RANGE = np.arange(0.1, 10, 0.1)
 
 
 def db_scan_clustering(data_set: np.ndarray, data_set_name: str,
                        is_euclidean_metric: bool, save_latex: bool = False) -> None:
-    for sample in MIN_SAMPLES_RANGE:
+    for min_sample in MIN_SAMPLES_RANGE:
         for epsilon in EPSILON_RANGE:
             db_scan: DBSCAN = DBSCAN(
-                min_samples=sample, eps=epsilon, p=2 if is_euclidean_metric else 1
+                min_samples=min_sample, eps=epsilon, p=2 if is_euclidean_metric else 1
             )
             cluster_labels = db_scan.fit_predict(data_set)
 
