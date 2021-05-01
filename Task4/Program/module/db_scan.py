c4183685f3093505bbb4576697169b58620825c5
Kamil Kowalewski <szamil24@gmail.com>
2021-05-01 13:28:41 +0200

    Add impl of db_scan

--- Task4/Program/module/db_scan.py
+++ Task4/Program/module/db_scan.py
@@ -2,9 +2,20 @@ import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_score
 
+EPSILON_RANGE = np.arange(0.1, 10, 0.1)
+MIN_SAMPLES_RANGE = np.arange(2, 10, 1)
+
 
 def db_scan_clustering(data_set: np.ndarray, data_set_name: str,
                        is_euclidean_metric: bool, save_latex: bool = False) -> None:
-    db_scan: DBSCAN = DBSCAN(eps=0.5, p=2 if is_euclidean_metric else 1)
-    y_db_scan = db_scan.fit_predict(data_set)
-    # print("Silhouette:\t" + str(silhouette_score(data_set, y_db_scan)))
+    for sample in MIN_SAMPLES_RANGE:
+        for epsilon in EPSILON_RANGE:
+            db_scan: DBSCAN = DBSCAN(
+                min_samples=sample, eps=epsilon, p=2 if is_euclidean_metric else 1
+            )
+            cluster_labels = db_scan.fit_predict(data_set)
+
+            is_all_items_same = np.all(cluster_labels == cluster_labels[0])
+            if not is_all_items_same:
+                score = round(silhouette_score(data_set, cluster_labels), 4)
+                print("Silhouette:\t" + str(score))
