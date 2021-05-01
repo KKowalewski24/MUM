511ff833db69838dbbcf346d2fc3b6fcb4fee283
Kamil Kowalewski <szamil24@gmail.com>
2021-05-01 16:54:31 +0200

    Add plotting coef

--- Task4/Program/module/db_scan.py
+++ Task4/Program/module/db_scan.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_score
@@ -14,6 +18,8 @@ latex_generator: LatexGenerator = LatexGenerator(LATEX_RESULTS_DIR)
 def db_scan_clustering(data_set: np.ndarray, data_set_name: str,
                        is_euclidean_metric: bool, save_latex: bool = False) -> None:
     for min_sample in MIN_SAMPLES_RANGE:
+        # Tuple[List[epsilon], List[silhouette_score]]
+        silhouette_scores: Tuple[List[float], List[float]] = ([], [])
         for epsilon in EPSILON_RANGE:
             db_scan: DBSCAN = DBSCAN(
                 min_samples=min_sample, eps=epsilon, p=2 if is_euclidean_metric else 1
@@ -23,4 +29,23 @@ def db_scan_clustering(data_set: np.ndarray, data_set_name: str,
             is_all_items_same = np.all(cluster_labels == cluster_labels[0])
             if not is_all_items_same:
                 score = round(silhouette_score(data_set, cluster_labels), 4)
+                silhouette_scores[0].append(epsilon)
+                silhouette_scores[1].append(score)
                 print("Silhouette:\t" + str(score))
+
+        plt.plot(silhouette_scores[0], silhouette_scores[1])
+        plt.title(
+            data_set_name + (", Euclidean" if is_euclidean_metric else ", Manhattan")
+            + " metric" + ", min sample: " + str(min_sample)
+        )
+        plt.xlabel("Epsilon")
+        plt.ylabel("Silhouette Score")
+
+        if save_latex:
+            filename_description = "_" + data_set_name \
+                                   + ("_eucl" if is_euclidean_metric else "_manh")
+            image_filename = filename_description + "-" + datetime.now().strftime("%H%M%S")
+
+            plt.close()
+
+        plt.show()
