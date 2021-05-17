4d77bf07524f5b6a634d350ff74d0b9d4c182736
Kamil Kowalewski <szamil24@gmail.com>
2021-05-17 11:40:22 +0200

    Change structure of config + add calculating accuracy

--- Task5/Program/Classification/main.py
+++ Task5/Program/Classification/main.py
@@ -1,9 +1,11 @@
 import subprocess
 import sys
 from argparse import ArgumentParser, Namespace
+from typing import Tuple
 
+import numpy as np
 from module.reader import read_gestures_ds, read_heart_ds, read_weather_AUS
-from sklearn import naive_bayes, svm
+from sklearn import metrics, naive_bayes, svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -15,34 +17,34 @@ Sample usage:
 """
 
 # TODO SET PROPER PARAMETERS
-classifier_configuration = {
-    "Heart": (read_heart_ds(), [
-        {"knn": KNeighborsClassifier(n_neighbors=9, p=2)},
-        {"bayes": naive_bayes.GaussianNB()},
-        # {"svm": svm.SVC(kernel=kernel_function, C=c, gamma=gamma)},
-        # {"decision_tree": DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,
-        #                                          max_depth=max_depth, random_state=47)},
-        # {"random_forest": RandomForestClassifier(n_jobs=-1, n_estimators=n_estimators,
-        #                                          **best_params, random_state=47)}
-    ]),
-    "Gestures": (read_gestures_ds(), [
-        {"knn": KNeighborsClassifier(n_neighbors=9, p=2)},
-        {"bayes": naive_bayes.GaussianNB()},
-        # {"svm": svm.SVC(kernel=kernel_function, C=c, gamma=gamma)},
-        # {"decision_tree": DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,
-        #                                          max_depth=max_depth, random_state=47)},
-        # {"random_forest": RandomForestClassifier(n_jobs=-1, n_estimators=n_estimators,
-        #                                          **best_params, random_state=47)}
-    ]),
-    "Weather": (read_weather_AUS(), [
-        {"knn": KNeighborsClassifier(n_neighbors=9, p=2)},
-        {"bayes": naive_bayes.GaussianNB()},
-        # {"svm": svm.SVC(kernel=kernel_function, C=c, gamma=gamma)},
-        # {"decision_tree": DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,
-        #                                          max_depth=max_depth, random_state=47)},
-        # {"random_forest": RandomForestClassifier(n_jobs=-1, n_estimators=n_estimators,
-        #                                          **best_params, random_state=47)}
-    ])
+classifiers_configuration = {
+    "Heart": (read_heart_ds(), {
+        "knn": KNeighborsClassifier(n_neighbors=9, p=2),
+        "bayes": naive_bayes.GaussianNB(),
+        # "svm": svm.SVC(kernel=kernel_function, C=c, gamma=gamma),
+        # "decision_tree": DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,
+        #                                         max_depth=max_depth, random_state=47),
+        # "random_forest": RandomForestClassifier(n_jobs=-1, n_estimators=n_estimators,
+        #                                         **best_params, random_state=47)
+    }),
+    "Gestures": (read_gestures_ds(), {
+        "knn": KNeighborsClassifier(n_neighbors=9, p=2),
+        "bayes": naive_bayes.GaussianNB(),
+        # "svm": svm.SVC(kernel=kernel_function, C=c, gamma=gamma),
+        # "decision_tree": DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,
+        #                                         max_depth=max_depth, random_state=47),
+        # "random_forest": RandomForestClassifier(n_jobs=-1, n_estimators=n_estimators,
+        #                                         **best_params, random_state=47)
+    }),
+    "Weather": (read_weather_AUS(), {
+        "knn": KNeighborsClassifier(n_neighbors=9, p=2),
+        "bayes": naive_bayes.GaussianNB(),
+        # "svm": svm.SVC(kernel=kernel_function, C=c, gamma=gamma),
+        # "decision_tree": DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,
+        #                                         max_depth=max_depth, random_state=47),
+        # "random_forest": RandomForestClassifier(n_jobs=-1, n_estimators=n_estimators,
+        #                                         **best_params, random_state=47)
+    })
 }
 
 
@@ -50,11 +52,28 @@ classifier_configuration = {
 def main() -> None:
     args = prepare_args()
     save_latex: bool = args.save
+    for config in classifiers_configuration:
+        display_header(config)
+        data_set = classifiers_configuration[config][0]
+        classifiers = classifiers_configuration[config][1]
+        for classifier in classifiers:
+            print(classifier)
+            calculate_metrics(data_set, classifiers[classifier])
+            print()
 
     display_finish()
 
 
 # DEF ------------------------------------------------------------------------ #
+def calculate_metrics(data_set: Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray],
+                      classifier) -> None:
+    X_train, X_test, y_train, y_test = data_set
+    classifier.fit(X_train, y_train)
+    y_prediction = classifier.predict(X_test)
+    accuracy = round(metrics.accuracy_score(y_test, y_prediction), 4)
+    print(accuracy)
+
+
 def display_header(name: str) -> None:
     print("------------------------------------------------------------------------")
     print(name)
