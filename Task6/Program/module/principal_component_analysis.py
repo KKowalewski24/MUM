d08afaab294eb45cd81041190afdf37633207884
Kamil Kowalewski <szamil24@gmail.com>
2021-05-29 17:25:45 +0200

    Add impl of PCA

--- Task6/Program/module/principal_component_analysis.py
+++ Task6/Program/module/principal_component_analysis.py
@@ -1,9 +1,23 @@
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 import numpy as np
+from sklearn.decomposition import PCA
+
+# TODO ADD REAL PARAMS
+COMPONENTS: List[float] = np.arange(2, 10, 1)
+SVD_SOLVERS: List[str] = ["auto", "full", "arpack"]
 
 
 def principal_component_analysis(
         datasets: Dict[str, Dict[str, Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]]],
         save_latex: bool = False) -> None:
-    pass
+    for ds_name in datasets:
+        X_train, X_test, y_train, y_test = datasets[ds_name]["original"]
+        for svd_solver in SVD_SOLVERS:
+            for component in COMPONENTS:
+                pca: PCA = PCA(n_components=component, svd_solver=svd_solver, random_state=21)
+                pca.fit(X_train)
+                X_train_transformed = pca.transform(X_train)
+                X_test_transformed = pca.transform(X_test)
+                key: str = "pca_" + svd_solver + "_" + str(component)
+                datasets[ds_name][key] = (X_train_transformed, X_test_transformed, y_train, y_test)
