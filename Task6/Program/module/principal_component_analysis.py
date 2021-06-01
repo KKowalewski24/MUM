5cdc8ea1d24a014669294be084dbe1bc2226a44f
Kamil Kowalewski <szamil24@gmail.com>
2021-06-01 20:55:35 +0200

    Add proper params

--- Task6/Program/module/principal_component_analysis.py
+++ Task6/Program/module/principal_component_analysis.py
@@ -3,9 +3,9 @@ from typing import Dict, List, Tuple
 import numpy as np
 from sklearn.decomposition import PCA
 
-# TODO ADD REAL PARAMS
-COMPONENTS: List[float] = np.arange(2, 10, 1)
-SVD_SOLVERS: List[str] = ["auto", "full", "arpack"]
+# Fractions from 0.3 to 0.95
+COMPONENTS_FRACTIONS: List[float] = np.arange(0.3, 1.0, 0.05).round(2)
+SVD_SOLVERS: List[str] = ["auto", "full", "arpack", "randomized"]
 
 
 def principal_component_analysis(
@@ -13,11 +13,19 @@ def principal_component_analysis(
         save_latex: bool = False) -> None:
     for ds_name in datasets:
         X_train, X_test, y_train, y_test = datasets[ds_name]["original"]
+
+        # Find min in shape - rows and cols number - this is required
+        # for proper working of PCA from sklearn
+        cols_number = min(X_train.shape[0], X_train.shape[1])
+
         for svd_solver in SVD_SOLVERS:
-            for component in COMPONENTS:
-                pca: PCA = PCA(n_components=component, svd_solver=svd_solver, random_state=21)
+            for components_percentage in COMPONENTS_FRACTIONS:
+                # fraction multiplied by number of cols in certain dataset
+                components_value: int = round(components_percentage * cols_number)
+                pca: PCA = PCA(n_components=components_value,
+                               svd_solver=svd_solver, random_state=21)
                 pca.fit(X_train)
                 X_train_transformed = pca.transform(X_train)
                 X_test_transformed = pca.transform(X_test)
-                key: str = "pca_" + svd_solver + "_" + str(component)
+                key: str = "pca_" + svd_solver + "_" + str(components_value)
                 datasets[ds_name][key] = (X_train_transformed, X_test_transformed, y_train, y_test)
